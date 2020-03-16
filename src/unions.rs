e4e561be4f85456345a815ee4504add91935dffb
Eric Caleb <caleberic@tuta.io>
2020-03-16 13:06:58 +0100

    add unions example

---
+++ src/unions.rs
@@ -0,0 +1,29 @@
+
+union IntOrFloat{
+    i: i32,
+    f: f32
+}
+
+fn process_value(iof: IntOrFloat){
+    unsafe {
+        match iof {
+            IntOrFloat {i: 42 } => {
+                println!("meaning of somethin value");
+            }
+
+            IntOrFloat {f} => {
+                println!("value = {}", f)
+            }
+        }
+    }
+}
+
+pub fn unions(){
+    let mut iof = IntOrFloat{ i: 123};
+    iof.i = 234;
+
+    let value = unsafe { iof.i };
+    println!("iof.i = {}", value);
+
+    process_value(IntOrFloat{f:42.0})
+}
