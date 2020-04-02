570f24c311db974f06a076a45537fa841b9fff4d
Eric Caleb <caleberic@tuta.io>
2020-04-02 10:03:20 +0200

    add alternative string mod

---
+++ src/statdis.rs
@@ -0,0 +1,31 @@
+
+trait Printable {
+    fn format(&self) -> String;
+}
+
+impl Printable for i32 {
+    fn format(&self) -> String {
+        format!("i32: {}", *self)
+    }
+}
+
+impl Printable for String {
+    fn format(&self) -> String {
+        format!("string: {}", *self)
+    }
+}
+
+fn print_it<T: Printable>(z: T){
+    println!("{}", z.format())
+}
+
+pub fn statdis(){   
+    let a = 123;
+    let b = "hello world".to_string();
+
+    // println!("{}", a.format());
+    // println!("{}", b.format());
+    print_it(a);
+    print_it(b);
+
+}
