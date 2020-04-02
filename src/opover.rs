68535f69f27856400b63941fcdd8837b5918c013
Eric Caleb <caleberic@tuta.io>
2020-04-02 10:25:10 +0200

    update operator overloading

--- src/opover.rs
+++ src/opover.rs
@@ -1,4 +1,6 @@
-use std::ops::{Add};
+// operator overloading
+use std::ops::{Add, AddAssign, Neg};
+use std::cmp::PartialEq;
 use std::fmt::Debug;
 
 #[derive(Debug)]
@@ -13,9 +15,51 @@ impl <T> Complex<T> {
     }
 }
 
-pub fn operator_overload(){
-    let mut a =  Complex::new(5, 1);
-    let mut a =  Complex::new(7, 3);
+impl<T> Add for Complex<T>  where T: Add<Output = T> {
+    type Output = Complex<T>;
 
-    println!("{:?}", a);
+    fn add(self , rhs: Self) -> Self::Output {
+        Complex {
+            re:  self.re + rhs.re,
+            im:  self.im + rhs.im
+        }
+    }
+}
+
+impl<T> AddAssign for Complex<T> where T: AddAssign<T> {
+    fn add_assign(&mut self, rhs: Self){
+        self.re  += rhs.re;
+        self.im  += rhs.im
+    }
+}
+
+impl<T> Neg for Complex<T> where T: Neg<Output=T> {
+    type Output = Complex<T>;
+
+    fn neg(self)  -> Self::Output {
+        Complex {
+            re: -self.re,
+            im: -self.im
+        }
+    }
+}
+
+impl<T> PartialEq for Complex<T> where T:PartialEq{
+    fn eq(&self , rhs: &Self) -> bool {
+        self.re == rhs.re && self.im == rhs.im
+    }
+}
+
+pub fn operator_overload(){
+    let a =  Complex::new(5, 1);
+    let b =  Complex::new(7, 3);
+
+    // println!("{:?}", a+b);
+
+    // a += b;
+    // println!("{:?}", a);
+
+    // println!("{:?}", -a);
+    println!("{:?}", a==a);
+
 }
