9dd80aa69c7be2acb385b78c8b5ab22b1bcdddeb
Eric Caleb <caleberic@tuta.io>
2020-04-02 10:26:01 +0200

    add into mod

---
+++ src/intos.rs
@@ -0,0 +1,24 @@
+struct Person {
+    name: String
+}
+
+impl Person { 
+    // fn new(name: &str) -> Person {
+    //     Person {
+    //         name: name.to_string()
+    //     }
+    // }
+    
+    // fn new<S: Into <String>>(name: S) -> Person {
+    fn new<S>(name: S) -> Person where S: Into<String> {
+        Person {name: name.into()}
+    }
+
+}
+
+pub fn intos(){
+    let j = Person::new("John");
+
+    let name:String  =  "Jane".to_string();
+    let jn = Person::new(name /*.as_ref()*/);
+}
